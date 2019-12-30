46402f7cf526fcb468910ea75a31dca45c960c7f
ksk001100 <hm.pudding0715@gmail.com>
2019-12-30 22:55:25 +0900

    fix

--- lib/pixel_matcher/diff_image.rb
+++ lib/pixel_matcher/diff_image.rb
@@ -1,29 +1,32 @@
 require 'rmagick'
 
-class PixelMatcher::DiffImage < Magick::Image
-  def initialize(path1, path2)
-    img1 = [].tap do |arr|
-      Magick::Image.read(path1).first.each_pixel do |pixel, c ,r|
-        arr << [pixel, c, r]
-      end
+module PixelMatcher
+  class DiffImage < Magick::Image
+    def initialize(path1, path2)
+      img1 = image_array(path1)
+      img2 = image_array(path2)
+      super(img1.last[1], img1.last[2])
+      store_diff(img1, img2)
     end
-    
-    img2 = [].tap do |arr|
-      Magick::Image.read(path2).first.each_pixel do |pixel, c ,r|
-        arr << [pixel, c, r]
+
+    def export(output_path)
+      write(output_path)
+    end
+
+    private
+
+    def store_diff(arr1, arr2)
+      arr1.zip(arr2) do |a1, a2|
+        store_pixels(a1[1], a1[2], 1, 1, [a1[0]]) if (a1[0] <=> a2[0]) != 0
       end
     end
 
-    super(img1.last[1], img1.last[2])
-
-    img1.zip(img2) do |i1, i2|
-      if (i1[0] <=> i2[0]) != 0
-        self.store_pixels(i1[1], i1[2], 1, 1, [i1[0]])
+    def image_array(path)
+      [].tap do |arr|
+        Magick::Image.read(path).first.each_pixel do |pixel, c, r|
+          arr << [pixel, c, r]
+        end
       end
     end
-  end
-
-  def export(output_path)
-    self.write(output_path)
   end
 end
